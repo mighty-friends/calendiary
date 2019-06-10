c9b58e1fa0e13c79238870abaef982160d1485aa
Yujin Gaya <yujin.gaya@gmail.com>
2019-06-10 16:13:49 +0900

    Add component `Day`

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="month">
+      <Day
+        v-for="n in 31"
+        :key="n"
+        class="day"
+        :style="{
+          top: `calc(${2 * Math.floor(n / 7)}rem + ${3 * Math.floor(n / 7)}px)`,
+          left: `calc(${2 * (n % 7)}rem + ${3 * (n % 7)}px)`
+        }"/>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import Day from './components/Day.vue'
+
+export default Vue.extend({
+  components: {
+    Day
+  }
+})
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+.month {
+  position: relative;
+  margin: 2rem;
+  .day {
+    position: absolute;
   }
 }
 </style>
---
+++ src/components/Day.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="day">
+  <div class="diary"></div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style lang="scss" scoped>
+$day-size: 2rem;
+$day-border-radius: 0.375rem;
+$day-hover-border: 0.125rem;
+$diary-size: 0.5rem;
+$diary-margin: ($day-size - $diary-size) / 2;
+
+.day {
+  height: $day-size;
+  width: $day-size;
+  background-color: whitesmoke;
+  border-radius: $day-border-radius;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    border: $day-hover-border solid #dbdbdb;
+    background-color: #efefef;
+  }
+
+  .diary {
+    height: $diary-size;
+    width: $diary-size;
+    border-radius: 50%;
+    background-color: #dbdbdb;
+  }
+}
+</style>
